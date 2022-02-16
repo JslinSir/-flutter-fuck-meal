0f8cbbaa543af04ec639ae80be7213a7dcde7f79
JsLin <oncwnuLXM1rj7_ui3G122mqdYLgI@git.weixin.qq.com>
2022-02-16 22:27:21 +0800

    add: 增加层叠布局 绝对定位

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import './layout/row/base.dart';
 import './layout/Row/demo1.dart';
 import './layout/container/demo1.dart';
 import './layout/text/text.dart';
+import './layout/stack/stack.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(title: 'Welcome to Flutter',
-     home: new TextEmp());
+     home: new StackEmp());
   }
 }
 
